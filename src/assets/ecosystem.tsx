acc9d2773d22b859c5eb5d895328017a2e6e8cb0
boilerrat <128boilerrat@gmail.com>
2023-09-01 20:36:48 -0400

    image rendering fix

--- src/assets/ecosystem.tsx
+++ src/assets/ecosystem.tsx
@@ -3,37 +3,37 @@ const Ecosystem = [
     title: "DAO Masons",
     description: "Bespoke Web3 solutions. Inspired by game design.",
     url: "https://daomasons.com",
-    image: "/public/DAOMasons.png"
+    image: "/DAOMasons.png"
   },
   {
     title: "Bootleggers",
     description: "Distilling the spirit of community into tools for coordination.",
     url: "https://bootleggers.wtf",
-    image: "/public/bootleggers.png"
+    image: "/bootleggers.png"
   },
   {
     title: "Yeet",
     description: "Raise Funds and Rally Your Community at the Push of a Button",
     url: "https://yeet.daohaus.club/",
-    image: "/public/yeet.png" 
+    image: "/yeet.png" 
   },
   {
     title: "Silo HAUS",
     description: "Crowdfund. Onboard. Strategize. Launch. On-chain multiplayer coordination games made easy.",
     url: "https://silo.haus/",
-    image: "/public/silo.png"
+    image: "/silo.png"
   },
   {
     title: "Protocol Guild",
     description: "A collective of Ethereum contributors working to boost the incentives around stewarding the core protocol.",
     url: "https://github.com/HausDAO/protocol-guild-contracts",
-    image: "/public/protocolguild.png" 
+    image: "/protocolguild.png" 
   },
   {
     title: "Raid Brood",
     description: "Pooling our Web3 powers to conspire against Moloch in taverns around the world.",
     url: "https://brood.raidguild.org/",
-    image: "/public/raidbrood.png" 
+    image: "/raidbrood.png" 
   },
   // Add more objects for additional cards
 ];
